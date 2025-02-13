b7e2fe9f9c7f27c33b9fc25e81e69ba9da0b7104
Quamin Hua <quaminhua56@gmail.com>
2025-02-13 15:08:32 -0800

    Create components folder with basic components
    Basic components include components used for the authentication screen.

---
+++ fetch-assessment/src/components/Heading/Title.tsx
@@ -0,0 +1,5 @@
+export default function Title({ children } : { children: React.ReactNode }) {
+    return (
+        <h1>{children}</h1>
+    )
+}
---
+++ fetch-assessment/src/components/InputBox/AuthenticationForm.tsx
@@ -0,0 +1,9 @@
+export default function AuthenticationForm() {
+    return (
+        <form>
+            <input type="text"></input>
+            <input type="email"></input>
+            <button>Log In</button>
+        </form>
+    )
+}
