06f83cd1f1d46daa12fcffbefb583b144a387f9d
Quamin Hua <quaminhua56@gmail.com>
2025-02-21 13:08:17 -0800

    Remove .ts extension from import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import * as api from './api.ts'
+import * as api from './api'
 import Title from './components/Title/Title'
 import AuthenticationForm from './components/AuthenticationForm/AuthenticationForm'
 import Header from './components/Header/Header'
